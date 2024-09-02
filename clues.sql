926b833d9edaa64a02ba8371d2b60bbd0fd41f46
Fadem300 <fadel.m200@gmail.com>
2024-09-02 09:43:22 +0300

    .

--- clues.sql
+++ clues.sql
@@ -42,7 +42,10 @@ SELECT countrycode
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
--- ?
+    -- SELECT cities.*
+  SELECT cities.name 
+   FROM cities , countries 
+     WHERE cities.countrycode = countries.code AND cities.name LIKE 'Serra%' AND continent = 'South America';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
